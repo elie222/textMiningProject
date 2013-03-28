e5fac3b350f4eb9aa3cd204b9153a190e8c843a5
Eliezer <Elie2@elies-macbook-2.local>
2013-03-28 20:15:43 +0200

    program now works with all our csv files

--- sentimentAnalyzer.py
+++ sentimentAnalyzer.py
@@ -5,13 +5,14 @@ def analyze(twit):
 
     return taggedTwit
 
-def convertCSVFileToArrayOfDicts(csvFileName):
+def convertCSVFileToArrayOfDicts(csvFileName, headers='first row'):
     arrayOfDicts = []
 
     with open(csvFileName, 'r') as f:
         reader = csv.reader(f)
 
-        headers = reader.next()
+        if headers == 'first row':
+            headers = reader.next()
 
         for row in reader:
             #creates a dict with headers as keys and row as values
@@ -24,10 +25,16 @@ def main():
     #example twit
     # twitData = '407487,@dustin I&amp;#39;m long $aapl or at least until $goog OS is out next year.,2,genevate,default,normal,http://avatars.stocktwits.com/production/2/thumb-1330556785.png?407487,https://s3.amazonaws.com/st-avatars/production/2/thumb-1330556785.png?407487,false,false,0,false,false,false,false,false,0,0,false,1,"686,2044","AAPL,GOOG",,1,dustin,Fri Jul 10 21:47:36 UTC 2009,Fri Jul 10 21:47:36 UTC 2009'
 
-    csvFileName = 'StockTwits-Data(beginning of file).csv'
-    twitsArray = convertCSVFileToArrayOfDicts(csvFileName)
 
-    print twitsArray[10]['body']
+    twitsArray = convertCSVFileToArrayOfDicts('StockTwits-Data(beginning of file).csv')
+
+    headers = ['sentence','n','score','keyword']#i don't know what n is
+    posSentimentArray = convertCSVFileToArrayOfDicts('wschemaPositive.xml.csv', headers)
+    negSentimentArray = convertCSVFileToArrayOfDicts('wschemaNegative.xml.csv', headers)
+
+    print twitsArray[0]
+    print posSentimentArray[0]
+    print negSentimentArray[0]
 
 if __name__ == '__main__':
     main()
