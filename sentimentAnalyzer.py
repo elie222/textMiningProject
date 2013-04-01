a61b93b68b4a561bf172a84545f167a31e92e995
Ofir Marcus <ofirule@gmail.com>
2013-04-01 20:29:33 +0300

    new analyzer not checked

--- sentimentAnalyzer.py
+++ sentimentAnalyzer.py
@@ -2,13 +2,30 @@ import sys
 import csv
 import copy
 import re
+import os
+from HTMLParser import HTMLParser
 from progressbar import ProgressBar
 
 class SentimentAnalyzer:
-    def __init__(self, pos_sentiment_array, neg_sentiment_array):
+    def __init__(self):
+        """
         self.pos_sentiment_array = pos_sentiment_array
         self.neg_sentiment_array = neg_sentiment_array
-
+        """
+        
+        SlangLookupTablePath = os.path.join(os.path.dirname(__file__), 'SentStrength_Data_Sept2011/SlangLookupTable.txt')
+        self.SlangLookupTableMap = buildMapFromFile(SlangLookupTablePath,"\t")
+        NegatingWordListPath = os.path.join(os.path.dirname(__file__), 'SentStrength_Data_Sept2011/NegatingWordList.txt')
+        self.NegatingWordSet = buildSetFromFile(NegatingWordListPath)
+        EmotionLookupTablePath = os.path.join(os.path.dirname(__file__), 'SentStrength_Data_Sept2011/EmotionLookupTable.txt')
+        self.EmotionLookupTableMap = buildMapFromFile(EmotionLookupTablePath,"\t")
+        EmoticonLookupTablePath = os.path.join(os.path.dirname(__file__), 'SentStrength_Data_Sept2011/EmoticonLookupTable.txt')
+        self.EmoticonLookupTableMap = buildMapFromFile(EmoticonLookupTablePath,"\t")
+        BoosterWordListPath = os.path.join(os.path.dirname(__file__), 'SentStrength_Data_Sept2011/BoosterWordList.txt')
+        self.BoosterWordMap = buildMapFromFile(BoosterWordListPath,"\t")
+        h = HTMLParser()
+        
+    """
     def analyze(self, twit_data):
         pos_score = 0
         neg_score = 0
@@ -38,7 +55,69 @@ class SentimentAnalyzer:
                 # print row['sentence']
 
         return pos_score - neg_score, tagged_twit
-
+    """
+    def analyze(self, twit_data):
+        score = 0
+        tagged_twit = twit_data['body']
+        fixed_twit = h.unescape(twit_data['body'])
+        for key in self.SlangLookupTableMap:
+            fixed_twit.replace(key,self.SlangLookupTableMap[key])
+        wordList = nltk.word_tokenize(fixed_twit)
+        
+        
+        twit_negating_word_index_set = set([])
+        twit_booster_word_index_to_score_map = {}
+        twit_emotion_word_index_to_score_map = {}
+        for i in xrange(__len__(wordList)):
+            if wordList[i] in self.NegatingWordSet:
+                twit_negating_word_index_set.add(i)
+            elif wordList[i] in self.BoosterWordMap:
+                twit_booster_word_index_to_score_map[i] = self.BoosterWordMap[key]
+            elif wordList[i] in self.EmoticonLookupTableMap:
+                score += int(self.EmoticonLookupTableMap[i])
+            else:
+                for key in self.EmotionLookupTableMap:
+                    if not re.match(key, wordList[i]) is None:
+                        twit_emotion_word_index_to_score_map[i] = self.EmotionLookupTableMap[key]
+        
+        for emotionalWordIndex in twit_emotion_word_index_to_score_map:
+            siman = 0
+            emScore = int(twit_emotion_word_index_to_score_map[emotionalWordIndex])
+            absScore = abs(emScore)
+            if emScore < 0:
+                siman = -1
+            else:
+                siman = 1
+            if i == 1:
+                if  (i - 1) in twit_booster_word_index_to_score_map:
+                    absScore += twit_booster_word_index_to_score_map[i - 1]
+                elif (i - 1) in twit_negating_word_index_set:
+                    absScore *= -1
+            elif i > 1:
+                if  (i - 1) in twit_booster_word_index_to_score_map:
+                    absScore += twit_booster_word_index_to_score_map[i - 1]
+                elif (i - 1) in twit_negating_word_index_set:
+                    absScore *= -1
+                    
+                if  (i - 2) in twit_booster_word_index_to_score_map:
+                    absScore += twit_booster_word_index_to_score_map[i - 1]
+                elif (i - 2) in twit_negating_word_index_set:
+                    absScore *= -1
+                
+            emScore = absScore * siman
+            
+            score += emScore
+            
+        if not re.match('!!!', fixed_twit ) is None:
+            siman = 0 
+            if score < 0:
+                siman = -1
+            else:
+                siman = 1
+            score = siman*(abs(score) + 1)
+        return score
+    
+    
 def convert_csv_file_to_array_of_dicts(csv_filename, headers='first row'):
     array_of_dicts = []
 
@@ -55,6 +134,27 @@ def convert_csv_file_to_array_of_dicts(csv_filename, headers='first row'):
 
     return array_of_dicts
 
+def buildMapFromFile (fname, sep):
+    retMap = {}
+    with open(fname) as f:
+        content = f.readlines()
+        for row in content:
+            splitRows = row.split(sep)
+            splitRows[1] = splitRows[1].replace('\n','')
+            splitRows[1] = splitRows[1].replace('\xa0','') 
+            retMap[splitRows[0]] = splitRows[1]           
+    return retMap
+    
+def buildSetFromFile (fname):
+    retSet = set([])
+    with open(fname) as f:
+        content = f.readlines()
+        for row in content:
+            fixedRow = row.replace('\n','')
+            fixedRow = fixedRow.replace('\xa0','')
+            retSet.add(fixedRow)           
+    return retSet
+    
 def clean_up_sentiment_array(array):
     clean_array = []
 
@@ -95,6 +195,7 @@ def clean_up_twits_array(twits_array):
 
 def main():
     # twits_array = convert_csv_file_to_array_of_dicts('StockTwits-Data(beginning of file).csv')
+    """
     twits_array = convert_csv_file_to_array_of_dicts('StockTwitsData1000.csv')
     # headers = ['id', 'body', 'user_id', 'user_login', 'message_source', 'message_type', 'avatar_url', 
     # 'avatar_url_ssl', 'investor_relations', 'private_relations', 'reply_count', 'reply_parent', 'chart', 
@@ -128,6 +229,8 @@ def main():
         i += 1
 
     pbar.finish()
+    """
+    sa = SentimentAnalyzer()
 
 
 if __name__ == '__main__':
