d8b5ec50990d4da5a6bdb479501f20df9fb5b359
Eliezer <Elie2@elies-macbook-2.local>
2013-04-07 18:58:58 +0300

    basic gui working

--- GUI.py
+++ GUI.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from sentimentAnalyzer import SentimentAnalyzer
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
@@ -11,15 +12,25 @@ class SA(QMainWindow):
         QMainWindow.__init__(self)
 
         self.initUI()
+        self.connectObjects()
 
     def initUI(self):
         twitLabel = QLabel('Twit:', self)
-        twitInput = QTextEdit(self)
+        self.twitInput = QTextEdit(self)
+        self.twitInput.setMaximumHeight(100)
+        tickerLabel = QLabel('Ticker Symbol:', self)
+        self.tickerInput = QLineEdit(self)
+        
+        vboxTwitDataInput = QVBoxLayout()
+        vboxTwitDataInput.addWidget(twitLabel)
+        vboxTwitDataInput.addWidget(self.twitInput)
+        vboxTwitDataInput.addWidget(tickerLabel)
+        vboxTwitDataInput.addWidget(self.tickerInput)
+
         self.getSentimentBtn = QPushButton('Get Sentiment', self)
 
         hboxTwitInput = QHBoxLayout()
-        hboxTwitInput.addWidget(twitLabel)
-        hboxTwitInput.addWidget(twitInput)
+        hboxTwitInput.addLayout(vboxTwitDataInput)
         hboxTwitInput.addWidget(self.getSentimentBtn)
 
 
@@ -39,15 +50,32 @@ class SA(QMainWindow):
         vbox.addLayout(hboxTwitInput)
         vbox.addLayout(hboxSentimentResult)
 
-
         mainWidget = QWidget()
         mainWidget.setLayout(vbox)
 
         self.setCentralWidget(mainWidget)
 
-        self.setGeometry(100, 100, 700, 400)
+        self.setGeometry(100, 100, 600, 200)
         self.setWindowTitle('Stock Twits Sentiment Analyzer')
 
+    def connectObjects(self):
+        self.getSentimentBtn.clicked.connect(self.getSentimentClicked)
+
+    def getSentimentClicked(self):
+        twitData = {}
+        twitData['body'] = str(self.twitInput.toPlainText())
+
+        #it doesn't actually matter what you put in here, since our SA gives every stock the same score anyway.
+        twitData['stock_symbols'] = str(self.tickerInput.displayText()).upper()
+
+        # print twitData['body']
+        # print twitData['stock_symbols']
+
+        sa = SentimentAnalyzer()
+        score = sa.analyze(twitData)
+        sentiment = sa.getSentiment(score)
+        self.sentimentResultLabel.setText(str(sentiment))
+        self.sentimentScoreLabel.setText(str(score))
 
 
 
--- sentimentAnalyzer.py
+++ sentimentAnalyzer.py
@@ -6,10 +6,10 @@ import os
 import nltk # http://nltk.org/install.html
 from HTMLParser import HTMLParser
 from progressbar import ProgressBar
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import matplotlib as mpl
-import datetime as dt
+# import matplotlib.pyplot as plt
+# import matplotlib.dates as mdates
+# import matplotlib as mpl
+# import datetime as dt
 
 
 
@@ -21,36 +21,6 @@ These are the headers in the StocksTwitFiles:
     'stock_symbols', 'in_reply_to_message_id', 'in_reply_to_user_id', 'in_reply_to_user_login', 'updated_at', 
     'created_at']
 To access the body of a twit you would write twit_data['body']. 
-'''
-
-'''
-BUGS on this line:
-absScore += int(twit_booster_word_index_to_score_map[i - 1])
-
-
-Maybe we should deal with this. I'm not sure.
-
-A list of twits with bad scores:
-
-TWIT: $INTC is the bomb.
-SCORE: -2
-Probably leave this one. Bomb could be bad.
-
-TWIT: $DRIV looks like it is breaking down from base.
-SCORE: 2
-The problem is probably with like. Maybe we should avoid looks like.
-
-TWIT: $WBSN looks like it is headed down for a gap fill.
-SCORE: 2
-The problem is probably with like. Maybe we should avoid looks like.
-
-TWIT: $CRY was a good one today sorry for not posting it sooner
-SCORE: -4
-What went wrong here? The word CRY? If so, we should deal with that. Shouldn't
-be checking things that start with $ or @.
-
-TWIT: $QQQQ up big on the year. don't fight the tape?
-SCORE: -3
 '''
 
 class SentimentAnalyzer:
@@ -73,14 +43,9 @@ class SentimentAnalyzer:
 
         # some other words:
         self.addMoreEmotionWords()
-        
-        # Needs to be self.h if you want to put it here.
-        # h = HTMLParser()
-        
 
     def analyze(self, twit_data):
         '''
-        TODO: Doesn't work ATM.
         Returns the score for the given twit data.
         '''
 
@@ -99,40 +64,30 @@ class SentimentAnalyzer:
         wordList = nltk.word_tokenize(fixed_twit)
 
         wordList = self.convertSlangToWords(wordList)
-        #print wordList
         
         twit_negating_word_index_set = set([])
         twit_booster_word_index_to_score_map = {}
         twit_emotion_word_index_to_score_map = {}
-
-        # print '===TWIT:', twit
 
         for i in xrange(len(wordList)):
             if i>0:
                 if wordList[i - 1] == '$':
                     continue
             if wordList[i] in self.NegatingWordSet:
-                # print 'NEGATING WORD:', wordList[i]
                 twit_negating_word_index_set.add(i)
             elif wordList[i] in self.BoosterWordMap:
-                # print 'BOOSTER WORD:', wordList[i]
                 twit_booster_word_index_to_score_map[i] = self.BoosterWordMap[wordList[i]]
             else:
                 for key in self.EmotionLookupTableMap:
                     if key.endswith('*'):
                         if re.match(key[:-1], wordList[i]) is not None:
                             twit_emotion_word_index_to_score_map[i] = self.EmotionLookupTableMap[key]
-                            #if not twit_emotion_word_index_to_score_map[i] == 0:
-                            #    print wordList[i], twit_emotion_word_index_to_score_map[i]
                     else:
                         if i > 0 and wordList[i].lower()=='like' and (wordList[i-1].lower() in ['look','looks']):
                             continue
                         if key == wordList[i].lower():
                             twit_emotion_word_index_to_score_map[i] = self.EmotionLookupTableMap[key]
-                            #if not twit_emotion_word_index_to_score_map[i] == 0:
-                            #    print wordList[i], twit_emotion_word_index_to_score_map[i]
-        #print twit_booster_word_index_to_score_map
-        #print twit_negating_word_index_set
+
         for i in twit_emotion_word_index_to_score_map:
             sign = 0
             emScore = int(twit_emotion_word_index_to_score_map[i])
@@ -142,7 +97,6 @@ class SentimentAnalyzer:
             else:
                 sign = 1
             if i == 1:
-                
                 if  (i - 1) in twit_booster_word_index_to_score_map:
                     absScore += int(twit_booster_word_index_to_score_map[i - 1])
                 elif (i - 1) in twit_negating_word_index_set:
@@ -157,16 +111,16 @@ class SentimentAnalyzer:
                     absScore += int(twit_booster_word_index_to_score_map[i - 2])
                 elif (i - 2) in twit_negating_word_index_set:
                     absScore *= -1
-            #print absScore   
+
             emScore = absScore * sign
-            ###looking for eomoticons
-            #print self.EmoticonLookupTableMap
-            for eomoticon in self.EmoticonLookupTableMap:
-                if not fixed_twit.find(eomoticon) == -1:
-                    emScore += int(self.EmoticonLookupTableMap[eomoticon])
+
+            ###looking for emoticons
+            for emoticon in self.EmoticonLookupTableMap:
+                if not fixed_twit.find(emoticon) == -1:
+                    emScore += int(self.EmoticonLookupTableMap[emoticon])
             score += emScore
            
-        if not re.search('!!!', fixed_twit ) is None:
+        if not re.search('!!!', fixed_twit) is None:
             sign = 0 
             if score < 0:
                 sign = -1
@@ -213,6 +167,82 @@ class SentimentAnalyzer:
 
         self.EmotionLookupTableMap['long'] = '4'
         self.EmotionLookupTableMap['short'] = '-4'
+
+    def tagBody(self, body, score):
+        retStr = ""
+        if score > 3:
+            retStr = "<very positive>" + body + "</very positive>"
+        elif score > 0:
+            retStr = "<positive>" + body + "</positive>"
+        elif score == 0:
+            retStr = "<neutral>" + body + "</neutral>"
+        elif score < -3:
+            retStr = "<very negative>" + body + "</very negative>"
+        elif score < 0:
+            retStr = "<negative>" + body + "</negative>"
+        return retStr
+
+    def getSentiment(self, score):
+        if score > 3:
+            return 'Very Positive'
+        elif score > 0:
+            return 'Positive'
+        elif score == 0:
+            return 'Neutral'
+        elif score < -3:
+            return 'Very Negative'
+        elif score < 0:
+            return 'Negative'
+
+    def converDate(strDate):
+        monthsMap = {'Jan': '01', 'Feb': '02', 'Mar': '03','Apr': '04','May': '05','Jun': '06','Jul': '07','Aug': '08','Sep': '09','Oct': '10','Nov': '11','Dec': '12'}
+        splitted = strDate.split(' ')
+        month= monthsMap[splitted[1]]
+        d = splitted[2] + '/' + month + '/' + splitted[5]
+        return dt.datetime.strptime(d,'%d/%m/%Y').date()
+        
+    def drawByCoordinates(self, x, posY, negY): 
+        
+        mpl.rcParams['axes.color_cycle'] = ['r', 'b']
+
+        #dates = ['01/02/1991','01/03/1991','01/04/1991','01/05/1991','03/05/1991','04/06/1991','06/06/1991','17/06/1991','22/06/1991']
+        #x = [dt.datetime.strptime(d,'%d/%m/%Y').date() for d in dates]
+
+        #posY = [3,7,90,3,55,78,99,3,25]
+        #negY = [2,6,77,8,98,12,32,5,17]
+
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))
+        plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+        plot = plt.plot(x,negY,x,posY)
+        plt.xlabel('dates')
+        plt.ylabel('sentiment')
+        plt.show(plot)
+
+    def drawGraph(self, ticker, startDate, endDate):
+        convStartDate = dt.datetime.strptime(startDate,'%d/%m/%Y').date()
+        convEndDate = dt.datetime.strptime(endDate,'%d/%m/%Y').date()
+        ticker = ticker.upper()
+        twits_array = convert_csv_file_to_array_of_dicts(ticker +'.csv')
+        date_score = {}
+        for twit_data in twits_array:
+            twitDate = converDate(twit_data['date'])
+            if twitDate > convStartDate and twitDate < convEndDate:
+                if not twitDate in date_score:
+                    date_score[twitDate] = [0,0]
+                if int(twit_data['score']) > 0:
+                    date_score[twitDate][0] += int(twit_data['score'])
+                else:
+                    date_score[twitDate][1] -= int(twit_data['score'])
+        keylist = date_score.keys()
+        keylist.sort()       
+        x = [i for i in keylist]
+        posY = [date_score[i][0] for i in keylist]
+        negY = [date_score[i][1] for i in keylist]
+        #print date_score
+        print x
+        print posY
+        print negY
+        drawByCoordinates(x, posY, negY)
 
     
 def convert_csv_file_to_array_of_dicts(csv_filename, headers='first row'):
@@ -328,21 +358,7 @@ def clean_up_twit(twit):
 
     return twit
 
-def tagBody(body, score):
-    retStr = ""
-    if score > 3:
-        retStr = "<very positive>" + body + "</very positive>"
-    elif score > 0:
-        retStr = "<positive>" + body + "</positive>"
-    elif score == 0:
-        retStr = "<neutral>" + body + "</neutral>"
-    elif score < -3:
-        retStr = "<very negative>" + body + "</very negative>"
-    elif score < 0:
-        retStr = "<negative>" + body + "</negative>"
-    return retStr
-    
-def getAllTickerTwits(filename, ticker, sa):
+def getAllTickerTwits(self, filename, ticker, sa):
     twits_array = convert_csv_file_to_array_of_dicts_for_ticker(filename, ticker)
     print len(twits_array)
     with open(ticker + ".csv", 'w') as f:
@@ -359,58 +375,6 @@ def getAllTickerTwits(filename, ticker, sa):
     print 'FINISHED:', ticker
 
 
-monthsMap = {'Jan': '01', 'Feb': '02', 'Mar': '03','Apr': '04','May': '05','Jun': '06','Jul': '07','Aug': '08','Sep': '09','Oct': '10','Nov': '11','Dec': '12'}
-
-def converDate(strDate):
-    splitted = strDate.split(' ')
-    month= monthsMap[splitted[1]]
-    d = splitted[2] + '/' + month + '/' + splitted[5]
-    return dt.datetime.strptime(d,'%d/%m/%Y').date()
-
-   
-    
-def drawByCoordinates(x, posY, negY): 
-    
-    mpl.rcParams['axes.color_cycle'] = ['r', 'b']
-
-    #dates = ['01/02/1991','01/03/1991','01/04/1991','01/05/1991','03/05/1991','04/06/1991','06/06/1991','17/06/1991','22/06/1991']
-    #x = [dt.datetime.strptime(d,'%d/%m/%Y').date() for d in dates]
-
-    #posY = [3,7,90,3,55,78,99,3,25]
-    #negY = [2,6,77,8,98,12,32,5,17]
-
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))
-    plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
-    plot = plt.plot(x,negY,x,posY)
-    plt.xlabel('dates')
-    plt.ylabel('sentiment')
-    plt.show(plot)
-
-def drawGraph(ticker, startDate, endDate):
-    convStartDate = dt.datetime.strptime(startDate,'%d/%m/%Y').date()
-    convEndDate = dt.datetime.strptime(endDate,'%d/%m/%Y').date()
-    ticker = ticker.upper()
-    twits_array = convert_csv_file_to_array_of_dicts(ticker +'.csv')
-    date_score = {}
-    for twit_data in twits_array:
-        twitDate = converDate(twit_data['date'])
-        if twitDate > convStartDate and twitDate < convEndDate:
-            if not twitDate in date_score:
-                date_score[twitDate] = [0,0]
-            if int(twit_data['score']) > 0:
-                date_score[twitDate][0] += int(twit_data['score'])
-            else:
-                date_score[twitDate][1] -= int(twit_data['score'])
-    keylist = date_score.keys()
-    keylist.sort()       
-    x = [i for i in keylist]
-    posY = [date_score[i][0] for i in keylist]
-    negY = [date_score[i][1] for i in keylist]
-    #print date_score
-    print x
-    print posY
-    print negY
-    drawByCoordinates(x, posY, negY)
     
 def main():
     """
@@ -464,6 +428,8 @@ def main():
     # getAllTickerTwits("INTC", sa)
     # getAllTickerTwits("MSFT", sa) 
     """
-    drawGraph('GOOG', '10/09/2009', '05/11/2009')
+    sa = SentimentAnalyzer()
+    sa.drawGraph('GOOG', '10/09/2009', '05/11/2009')
+
 if __name__ == '__main__':
     main()
